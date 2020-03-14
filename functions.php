15d4333a87836cc4e4ea31fbcf9161a6b998dd86
Yonkov <yonkov.atanas@gmail.com>
2020-03-14 02:13:01 +0200

    fix theme prefixes

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
-function fabian_hediger_enqueue_theme_styles() {
+function nasio_child_enqueue_theme_styles() {
     //Register parent theme's styles
    wp_enqueue_style( 'nasio-css', get_template_directory_uri().'/style.min.css' );
    //Register child theme's styles
-   wp_enqueue_style('fabian-theme', get_stylesheet_directory_uri() .'/style.css');
+   wp_enqueue_style('nasio-child', get_stylesheet_directory_uri() .'/style.css');
 }
-add_action( 'wp_enqueue_scripts', 'fabian_hediger_enqueue_theme_styles' );
+add_action( 'wp_enqueue_scripts', 'nasio_child_enqueue_theme_styles' );
 
 /* Add your child theme's code snippets below */
